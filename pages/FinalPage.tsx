3d8e4e21dfe372ab57299f4a81b11daa37c1b667
gorbenco03 <chiril.gorbenco.em@gmail.com>
2024-04-23 04:13:57 +0300

    qr code for both ways and all passengers

--- pages/FinalPage.tsx
+++ pages/FinalPage.tsx
@@ -1,71 +1,91 @@
+
+
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Alert, TouchableOpacity, ScrollView } from 'react-native';
 import QRCode from 'react-native-qrcode-svg';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import * as FileSystem from 'expo-file-system';
 
 const FinalPage = ({ route }: any) => {
   const { travelDetails } = route.params;
-  const [qrData, setQrData] = useState('');
+  const [qrData, setQrData] = useState<Array<string>>([]);
 
   useEffect(() => {
     if (travelDetails) {
-      const qrString = JSON.stringify(travelDetails);
-      setQrData(qrString);
+      const qrDataArray: Array<string> = [];
+
+      travelDetails.passengers.forEach((passenger: any) => {
+        const qrStringOutbound = JSON.stringify({ ...travelDetails, tripType: 'Outbound', passenger });
+        qrDataArray.push(qrStringOutbound);
+
+        if (travelDetails.returnDate) {
+          // Invert "from" and "to" for return trip
+          const qrStringReturn = JSON.stringify({
+            ...travelDetails,
+            tripType: 'Return',
+            passenger,
+            from: travelDetails.to,
+            to: travelDetails.from,
+          });
+          qrDataArray.push(qrStringReturn);
+        }
+      });
+
+      setQrData(qrDataArray);
     }
   }, [travelDetails]);
 
-  const saveQRAsImage = () => {
-    if (this.qr) {
-      this.qr.toDataURL((data: any) => {
-        FileSystem.writeAsStringAsync(FileSystem.documentDirectory + 'qrCode.png', data, { encoding: FileSystem.EncodingType.Base64 })
-          .then((fileUri) => {
-            Alert.alert('Save successful', `File saved to: ${fileUri}`);
-          })
-          .catch(error => {
-            console.error(error);
-            Alert.alert('Error', 'Failed to save QR code as image.');
-          });
+  const saveQRAsImage = (data: string, filename: string) => {
+    FileSystem.writeAsStringAsync(FileSystem.documentDirectory + filename, data, { encoding: FileSystem.EncodingType.Base64 })
+      .then((fileUri) => {
+        Alert.alert('Save successful', `File saved to: ${fileUri}`);
+        console.log({ fileUri })
+      })
+      .catch(error => {
+        console.error(error);
+        Alert.alert('Error', 'Failed to save QR code as image.');
       });
-    }
+  };
+
+  const handleAddToCalendar = () => {
+    // Implementare pentru adăugare în Calendar
   };
 
   const handleAddToApplePay = () => {
     // Implementare pentru adăugare în Apple Pay
-  }
-
-  const handleAddToCalendar = () => {
-    // Implementare pentru adăugare în Calendar
-  }
+  };
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.headerText}>Booking Confirmed</Text>
-      {qrData && (
-        <View style={styles.section}>
-          <QRCode
-            getRef={(c) => (this.qr = c)}
-            value={qrData}
-            size={200}
-          />
-          <Text style={styles.ticketText}>Show this QR code at the boarding gate.</Text>
-        </View>
-      )}
-      <TouchableOpacity style={styles.closeButton} onPress={saveQRAsImage}>
-        <Icon name="payment" size={20} color="#fff" />
-        <Text style={styles.searchButtonText}>Add to Calendar</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.closeButton} onPress={handleAddToCalendar}>
-        <Icon name="event" size={20}  color="#fff" />
-        <Text style={styles.searchButtonText}>Add to Calendar</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.closeButton} onPress={handleAddToApplePay}>
-        <Icon name="apple" size={20} color="#fff" />
-        <Text style={styles.searchButtonText}>Add to Calendar</Text>
-      </TouchableOpacity>
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.headerText}>Booking Confirmed</Text>
+        {qrData.map((qrString, index) => (
+          <View key={index} style={styles.section}>
+            <Text style={styles.sectionHeader}>Bilet pentru {travelDetails.passengers[index].name} {travelDetails.passengers[index].surname}</Text>
+            <QRCode
+              value={qrString}
+              size={200}
+            />
+            <Text style={styles.ticketText}>Prezentati acest QR la sofer.</Text>
+            <TouchableOpacity style={styles.closeButton} onPress={() => saveQRAsImage(qrString, `qrCode_${index}.png`)}>
+              <Icon name="payment" size={20} color="#fff" />
+              <Text style={styles.searchButtonText}>Descarca biletul</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.closeButton} onPress={handleAddToCalendar}>
+              <Icon name="event" size={20} color="#fff" />
+              <Text style={styles.searchButtonText}>Adauga in calendar</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.closeButton} onPress={handleAddToApplePay}>
+              <Icon name="apple" size={20} color="#fff" />
+              <Text style={styles.searchButtonText}>Adauga la Apple Pay</Text>
+            </TouchableOpacity>
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 };
+
 
 export default FinalPage;
 
@@ -83,9 +103,10 @@ const styles = StyleSheet.create({
     padding: 10,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     marginBottom: 5, // Spațiu între butoane
-    width: '80%',
+    width: '70%',
+    marginHorizontal:5,
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -99,7 +120,8 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontSize: 16,
     fontWeight: 'bold',
-    marginLeft: 10,
+    marginHorizontal:10, 
+
   },
   headerText: {
     fontSize: 22,
@@ -107,10 +129,17 @@ const styles = StyleSheet.create({
     color: '#333',
     marginBottom: 20,
   },
+  sectionHeader: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    textAlign: 'center',
+    color: '#333', // sau altă culoare pe care o dorești
+  },
   ticketText: {
     fontSize: 16,
     color: '#333',
-    marginTop: 20,
+    marginVertical: 20,
   },
   section: {
     backgroundColor: '#CBF1F5',
